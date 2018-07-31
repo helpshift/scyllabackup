444f749381985cd4bde8e26a1310a526ab9d1be4
Ayush Goyal <ayush@helpshift.com>
2018-07-31 16:12:34 +0530

    Bump version to 0.0.2
    * Updated db schema to reduce disk utilization
    * Added a snapshot verify function
    * Various small consistency/bugs/feature fixes
    
    Change-Id: Icbb1cc47121d94ec6bb89cd14e64481dd59d9083
    JIRA: https://helpshift.atlassian.net/browse/OPS-2311

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='scyllabackup',
-      version='0.0.1',
+      version='0.0.2',
       description='Scyllabackup: A tool for taking scylla backups',
       url='',
       license='Proprietary',
